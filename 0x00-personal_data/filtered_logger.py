ef2ded6a25ce972f1f56a5ef9855b4509cc774f6
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-11-23 05:29:50 +0000

    filtering logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,27 @@
 
 """ Personal data for users """
 
+import logging
 import re
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initialization of class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ custom formating with filter_datum function"""
+        s = super().format(record)
+        s = filter_datum(self.fields, self.REDACTION, s, self.SEPARATOR)
+        return (s)
 
 
 def filter_datum(
