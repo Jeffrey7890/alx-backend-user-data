4c39b23034458360cfcd53ed3945dba1741785c7
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-11-23 06:40:28 +0000

    connecting to a secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,10 @@
 """ Personal data for users """
 
 import logging
+import mysql.connector # type: ignore
+import os
 import re
-from typing import List
+from typing import List, Tuple
 
 PII_FIELDS = ('password', 'email', 'ssn', 'Name', 'phone')
 
@@ -15,7 +17,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: Tuple[str, ...]):
         """ Initialization of class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
@@ -28,7 +30,7 @@ class RedactingFormatter(logging.Formatter):
 
 
 def filter_datum(
-        fields: List[str], redaction: str,
+        fields: Tuple[str, ...], redaction: str,
         message: str, separator: str) -> str:
     """ returns obfuscated log message """
     for f in fields:
@@ -48,3 +50,24 @@ def get_logger() -> logging.Logger:
     logger.addHandler(ch)
     logger.propagate = False
     return (logger)
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ connecting to a secure database """
+
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    if user is None:
+        user = 'root'
+    if password is None:
+        password = ''
+    if host is None:
+        host = 'localhost'
+    mydb = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database='my_db'
+            )
+    return mydb
