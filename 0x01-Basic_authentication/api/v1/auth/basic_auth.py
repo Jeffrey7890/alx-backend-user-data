8a1bff086ccdde49a536b208d627f9e4a49af227
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-11-25 12:43:11 +0000

    advanced stuff

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -51,8 +51,12 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        credentials = decoded_base64_authorization_header.split(':')
-        return credentials[0], credentials[1]
+        # credentials = decoded_base64_authorization_header.split(':')
+        # return credentials[0], credentials[1]
+        L = decoded_base64_authorization_header.find(':')
+        email = decoded_base64_authorization_header[:L]
+        pwd = decoded_base64_authorization_header[L+1:]
+        return email, pwd
 
     def user_object_from_credentials(
             self,
