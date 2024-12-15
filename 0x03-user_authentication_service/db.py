06fc63c32e2abceef912801b2fe9f310c9ee46a9
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-12-15 17:38:51 +0000

    Create a find_user() function in DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base
 from user import User
@@ -39,3 +40,15 @@ class DB:
         self._session.add(user_)
         self._session.commit()
         return user_
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user from database """
+        try:
+            our_user = self._session.query(User).filter_by(**kwargs).first()
+            if our_user is None:
+                raise NoResultFound('Not found')
+            return our_user
+        except NoResultFound as e:
+            raise NoResultFound(str(e))
+        except InvalidRequestError as e:
+            raise InvalidRequestError(e)
