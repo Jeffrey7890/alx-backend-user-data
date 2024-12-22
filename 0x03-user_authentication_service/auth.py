4994ecef65491b80d1615f606206ff3d3c650b26
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-12-22 22:32:31 +0000

    Create an Auth class in auth.py
    To abstract authentication of users
    Auth class has register_user() function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,30 @@
 """ authorization helper functions """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ registers user in
+        database
+        """
+        try:
+            u = self._db.find_user_by(email=email)
+            if u is not None:
+                raise ValueError(f'{email} already exists')
+        except NoResultFound:
+            h_pwd = _hash_password(password)
+            user = self._db.add_user(email, h_pwd)
+            return user
 
 
 def _hash_password(password: str) -> bytes:
