272eb79214cc9f32f9961adce7fbdd64f34af7c5
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-12-23 01:16:59 +0000

    Creat a /users route in flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 """ basic flask app """
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
+AUTH = Auth()
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """
+    register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        usr = AUTH.register_user(email, password)
+        if usr is not None:
+            return jsonify({
+                "email": usr.email,
+                "message": "user created"})
+    except ValueError as e:
+        return jsonify({"message": "email alredy registered"}), 400
 
 
 @app.route("/")
