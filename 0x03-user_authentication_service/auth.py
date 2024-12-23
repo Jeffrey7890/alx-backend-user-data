502ca0430315779e8957f7a3d030d7f0339be37b
Jeffrey7890 <d3v3loperpy@gmail.com>
2024-12-23 01:26:42 +0000

    Validate user login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,15 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ validate user login """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+
     def register_user(self, email: str, password: str) -> User:
         """ registers user in
         database
